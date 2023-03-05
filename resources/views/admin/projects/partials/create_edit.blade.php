f068a23730c8f014677ec0ee4cdfd619e94bae43
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-05 11:40:45 +0100

    add: unico form create_edit

---
+++ resources/views/admin/projects/partials/create_edit.blade.php
@@ -0,0 +1,61 @@
+<form class="p-5" action="{{route('admin.projects.store')}}" method="POST" enctype="multipart/form-data">
+    @csrf
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+              @foreach($errors->all() as $error)
+                <li>
+                  {{ $error }}
+                </li>
+              @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="mb-3">
+        <label class="form-label">Titolo</label>
+        <input  type="text" class="form-control" name="title" value="{{ old('title', $project->title) }}" id="" >
+    </div>
+
+    <div class="mb-3">
+        <label for="form-label">Tipo</label>
+        <select class="form-control" name="type_id"  id="type_id">
+            @foreach ($types as $type)
+            <option value="{{ $type->id }}">{{ $type->type }}</option>
+            @endforeach
+        </select>
+    </div> 
+
+    <div class="row form-group">
+        <p>Tecnologie</p>
+        <div class="col-12">
+            @foreach ($tecnologies as $tecnology)
+            <input type="checkbox" class="form-check-input" name="tecnologies[]" value="{{ $tecnology->id }}">
+            @if ($errors->any())
+              @checked(in_array($tecnology->id, old('tecnologies', [])))
+            @else
+              @checked($project->tecnologies->contains($tecnology->id))
+            @endif
+            <label class="form-check-label">{{ $tecnology->tecnology }}</label>
+            @endforeach
+        </div>
+    </div>
+
+     <div class="mb-3">
+        <label class="form-label">Descrizione</label>
+        <textarea type="text" id="" name="description"  class="form-control" >{{ old('description', $project->description) }}</textarea>
+     </div>
+
+    <div class="mb-3">
+        <label class="form-label">Anno del progetto</label>
+        <input type="text" class="form-control" name="year_project" value="{{ old('year_project', $project->year_project) }}" id="" >  
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Inserisci un immagine</label>
+        <input type="file" class="form-control" name="image" value="{{ old('image', $project->image) }}" id="" >
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
