0354b82c77867be722a5382eb19ed25a1d8460de
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-05 11:49:41 +0100

    add: tecnology in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -11,6 +11,11 @@
                         <h1>
                             {{ $project->title }}
                         </h1>
+                        @forelse ($project->tecnologies as $tecnology)
+                        <span>{{ $tecnology->tecnology }}</span>
+                        @empty
+                        <p>Non ci sono tecnologie da mostrare</p> 
+                        @endforelse
                     </div>
                     <div class="card-img text-light">
                         <img src="{{ asset('storage/' . $project->image )}}" class="img-fluid" alt="">
